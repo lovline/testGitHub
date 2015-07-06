840131d60cd7064d6645dc16f87f52ee9fd0c1c2
lovline <1079974085@qq.com>
2015-07-06 14:07:09 +0800

    this is a dictory

---
+++ MyJob#askForLeave/src/com/tskjsx/askForLeave/test01.java
@@ -0,0 +1,16 @@
+package com.tskjsx.askForLeave;
+
+public class test01 {
+
+	/**
+	 * 
+	 * this is the first mask
+	 * askForLeave01
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
